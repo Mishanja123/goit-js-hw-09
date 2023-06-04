25b82623a1cee0eee16032322ad122e3fda5af18
Mishanja123 <116510947+Mishanja123@users.noreply.github.com>
2023-06-04 09:11:32 +0000

    Deploying to gh-pages from @ Mishanja123/goit-js-hw-09@a4de8d840d7c749eeb605ce610d78efc7b0e4409 🚀

---
+++ 01-color-switcher.060152a8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");function n(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),color.textContent=document.body.style.background}t.addEventListener("click",(function(){colorInterval=setInterval(n,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(colorInterval),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.060152a8.js.map
