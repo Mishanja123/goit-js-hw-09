81a220573a31b3e929edde994b2930e0659d0ddd
Misha <shevchenkom606@gmail.com>
2023-06-08 13:41:19 +0200

    promises fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -23,21 +23,28 @@ function createPromise(position, delay) {
 
 function onFormSubmit(e) {
   e.preventDefault();
+
   const delay = e.target.delay;
   const step = e.target.step;
   const amount = e.target.amount;
 
   let delayEl = parseInt(delay.value);
   let stepEl = parseInt(step.value);
-
-  for (let i = 0; i < amount.value; i ++) {
-    createPromise(i + 1, delayEl)
-      .then(({ position, delay }) => {
-        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-    delayEl += stepEl;
+  let amountEl = parseInt(amount.value);
+  if (delayEl < 0 || stepEl < 0 || amountEl <= 0) {
+    Notiflix.Notify.warning(`Please put positive value`);
   }
+  else {
+    for (let i = 0; i < amount.value; i++) {
+      createPromise(i + 1, delayEl)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+      delayEl += stepEl;
+    }
+  }
+  formEl.reset();
     }
